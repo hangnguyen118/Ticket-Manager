438e02b493d4a6f7a6e2c94a4e40291f3a384357
hangnguyen118 <128979595+hangnguyen118@users.noreply.github.com>
2025-02-16 19:09:35 +0700

    Hmcl 14 set up the backend repository (#11)
    * create back-end
    
    * create config backend
    
    * add change cache
    
    * update
    
    * update config
    
    * add new repository for backend
    
    * update config for gitgnore and path of repository vs
    
    * update config

--- back-end/TicketManager/obj/Debug/net9.0/TicketManager.AssemblyInfo.cs
+++ back-end/TicketManager/obj/Debug/net9.0/TicketManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ae499358b530e084696801bbdd5c59af03f9803")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
