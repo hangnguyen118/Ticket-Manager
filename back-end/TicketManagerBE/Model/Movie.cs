97a982ea6b6028c2874fef95d70a3910498b5fac
hangnguyen118 <128979595+hangnguyen118@users.noreply.github.com>
2025-02-16 18:34:23 +0700

    Hmcl 14 set up the backend repository (#8)
    * create back-end
    
    * create config backend
    
    * add change cache
    
    * update
    
    * update config

---
+++ back-end/TicketManagerBE/Model/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace TicketManagerBE.Model
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public DateTime? Created { get; set; } = null;
+    }
+}
