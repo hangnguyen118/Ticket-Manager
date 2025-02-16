2ae499358b530e084696801bbdd5c59af03f9803
hangnguyen118 <hangnguyen118.wy@gmail.com>
2025-02-16 19:06:56 +0700

    update config

--- back-end/TicketManager/obj/Debug/net9.0/TicketManager.AssemblyInfo.cs
+++ back-end/TicketManager/obj/Debug/net9.0/TicketManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ce082da88e3883a48ba26ef6173b96d4cb42c5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c474a0ff9bb9465c8b28bbba5914e1800dbf4f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
