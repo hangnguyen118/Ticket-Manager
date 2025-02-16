8a0938057d77c3e7777f4c21e8f2790f163b01d5
hangnguyen118 <hangnguyen118.wy@gmail.com>
2025-02-16 18:51:53 +0700

    add new repository for backend

---
+++ back-end/TicketManager/Program.cs
@@ -0,0 +1,26 @@
+
+namespace TicketManager
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
