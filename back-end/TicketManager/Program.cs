1ce082da88e3883a48ba26ef6173b96d4cb42c5e
hangnguyen118 <128979595+hangnguyen118@users.noreply.github.com>
2025-02-16 18:56:36 +0700

    Hmcl 14 set up the backend repository (#10)
    * create back-end
    
    * create config backend
    
    * add change cache
    
    * update
    
    * update config
    
    * add new repository for backend

--- back-end/TicketManager/Program.cs
+++ back-end/TicketManager/Program.cs
@@ -1,5 +1,5 @@
 
-namespace TicketManagerBE
+namespace TicketManager
 {
     public class Program
     {
@@ -10,16 +10,8 @@ namespace TicketManagerBE
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
 
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
 
             app.UseHttpsRedirection();
 
